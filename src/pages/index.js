ec6c0a36a5a4d40b2788d35f6daf15f7f307d0e1
Della <humanita.1999@gmail.com>
2020-12-02 15:12:20 -0700

    feat: created about.js page to test links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
+import { Link } from "gatsby"
+
 export default function Home() {
     
-  return <h1>Hello Gatsby!</h1>
+  return (
+
+    <main>
+      <h1>Hello Gatsby!</h1>
+      <p>
+        <Link to="/about/">About</Link>
+      </p>
+    </main>
+
+  )
 }
